f780bd1b2f2106343c673b9db5170011577a71b1
daniel calliari <dbcalliari@gmail.com>
2026-06-15 14:57:31 -0300

    chore: remove GitLab CI configuration and update Spring Boot test class to specify application context

--- src/test/java/com/example/bicos/BicosApplicationTests.java
+++ src/test/java/com/example/bicos/BicosApplicationTests.java
@@ -1,9 +1,10 @@
 package com.example.bicos;
 
+import com.example.demo.BicosApplication;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = BicosApplication.class)
 class BicosApplicationTests {
 
 	@Test
