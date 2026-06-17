cc8a8de7a7f9c36adcfacb0002be88425624a41e
davipcosta <dpimentel.costa@gmail.com>
2026-06-17 14:22:46 -0300

    feat(security): user can access /api/v1/bicos without logging

--- src/main/java/com/example/demo/bicos/infra/config/SecurityConfig.java
+++ src/main/java/com/example/demo/bicos/infra/config/SecurityConfig.java
@@ -38,6 +38,7 @@ public class SecurityConfig {
                 .authorizeHttpRequests(authorize -> authorize
                         .requestMatchers(HttpMethod.POST, "/auth/login").permitAll()
                         .requestMatchers(HttpMethod.POST, "/auth/register").permitAll()
+                        .requestMatchers(HttpMethod.GET, "/api/v1/bicos").permitAll()
                         .requestMatchers("/v3/api-docs/**","/swagger-ui/**","/swagger-ui.html").permitAll()
                         .requestMatchers("/health/**").permitAll()
                         .anyRequest().authenticated()
