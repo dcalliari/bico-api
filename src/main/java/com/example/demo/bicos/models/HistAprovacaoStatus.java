343f557a74e64768dc0a0714c016366e4d249d48
davipcosta <dpimentel.costa@gmail.com>
2026-07-06 14:30:06 -0300

    feat(migration): added migration to constraint candidatura_check_role

--- src/main/java/com/example/demo/bicos/models/HistAprovacaoStatus.java
+++ src/main/java/com/example/demo/bicos/models/HistAprovacaoStatus.java
@@ -3,11 +3,16 @@ package com.example.demo.bicos.models;
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonFormat;
 
-@JsonFormat(shape= JsonFormat.Shape.OBJECT)
+@JsonFormat(shape = JsonFormat.Shape.OBJECT)
 public enum HistAprovacaoStatus {
-    
+
     APROVADO("A", "Aprovado"),
-    REJEITADO ("R","Rejeitado");
+    REJEITADO("R", "Rejeitado"),
+    PENDENTE("P", "Pendente"),
+    APROVADO_N2("N2", "Aprovado N2"),
+    APROVADO_N3("N3", "Aprovado N3"),
+    DEVOLVIDO("D", "Devolvido"),
+    REENVIADO("RE", "Reenviado");
 
     private final String codigo;
     private final String descricao;
@@ -20,12 +25,13 @@ public enum HistAprovacaoStatus {
     public String getCodigo() {
         return codigo;
     }
+
     public String getDescricao() {
         return descricao;
     }
 
     @JsonCreator
-    public static HistAprovacaoStatus doValor(String codigo){
+    public static HistAprovacaoStatus doValor(String codigo) {
         if (codigo == null) {
             throw new IllegalArgumentException("Código não pode ser null");
         }
